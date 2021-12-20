1d34ddd3273a4deca3dd9f54a43e22b4ca2c0005
Moonling <52844176+VelouriasMoon@users.noreply.github.com>
2021-12-20 17:34:24 -0400

    Fixed relic unreal version

--- QueenIO/Relic.cs
+++ QueenIO/Relic.cs
@@ -10,7 +10,11 @@ namespace QueenIO
 {
     public class Relic : UAsset
     {
-        public new UE4Version EngineVersion = UE4Version.VER_UE4_18;
+        public Relic()
+        {
+            EngineVersion = UE4Version.VER_UE4_18;
+        }
+
         public DataTableExport GetDataTable()
         {
             return (DataTableExport)Exports[0];
